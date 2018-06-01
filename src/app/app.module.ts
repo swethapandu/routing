a9b53d38f34472d3914e67fe4ba76ef24f592347
K <571400@cognizant.com>
2018-06-01 17:20:23 +0530

    routing files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { UserComponent } from './user/user.component';
+import { UserListComponent } from './user-list/user-list.component';
+import {RouterModule, Routes} from '@angular/router';
+
+const appRoutes : Routes =[
+  {path:'home', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'user', children :[
+     {path:'list', component:UserListComponent, children: [
+       {path:'details/:name', component:UserComponent}
+     ]}
+  ]},
+  {path:'', redirectTo:'/home',pathMatch:'full'},
+  {path: '**', redirectTo:'/home',pathMatch:'full'}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    UserComponent,
+    UserListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
